53a48547e265c5b45c3d4598dd6fcf4c12472dc1
Elijah Insua <tmpvar@gmail.com>
2010-08-28 04:05:29 -0400

    selector almost works

--- public/js/selector.js
+++ public/js/selector.js
@@ -1,5 +1,6 @@
 (function($) {
- 
+  var annotations = [];
+
   function theInterceptor(ev) {
     ev.preventDefault();
     return false;
@@ -7,11 +8,16 @@
   $("a").click(theInterceptor);
 
   $(document).mousedown(function onDown(ev) {
+
+    // unbind the document.mousedown until this annotation is finished
+    $(document).unbind("mousedown", onDown);
+
     if ($(ev.target).parents().andSelf().hasClass("clickable")) {
       return;
     }
     var anchor = { x: ev.clientX, y: ev.clientY },
         bounds = { x: ev.clientX, y: ev.clientY },
+        origTarget = ev.target,
       
         // TODO: please god no!
         sel     = $('<canvas class="clickable selection"></canvas>'),
@@ -27,12 +33,12 @@
       widht: 0
     });    
   
-    function onMove(ev) {
+    function onMove(moveEvent) {
       sel.css({
-        width: ev.clientX - anchor.x,
-        height: ev.clientY - anchor.y
+        width: moveEvent.clientX - anchor.x,
+        height: moveEvent.clientY - anchor.y
       });
-      bounds = { x: ev.clientX, y: ev.clientY };
+      bounds = { x: moveEvent.clientX, y: moveEvent.clientY };
     }
     $(window).mousemove(onMove);
     $(window).one("mouseup", function onUp(ev) {
@@ -45,7 +51,39 @@
       });
 
       $("body").append(annotate); 
-      
+
+      $(window).click(function removeFocus(ev) {
+        if ($(ev.target).parents().andSelf().hasClass("clickable")) {
+          return;
+        } 
+        // re-bind the document.mousedown so we can handle another annotation!
+        $(document).mousedown(onDown);
+        $(".clickable").remove();
+        $(window).unbind("click", removeFocus);
+        
+        // add this citation to the list
+        var citation = {
+          node : {
+            path : path_node(origTarget),
+            hash : hash_node(origTarget)
+          },
+          text : $("textarea", annotate).val(),
+          bounds : {
+            anchor : {
+              x: anchor.x,
+              y: anchor.y
+            },
+            bounds : {
+              x: bounds.x,
+              y: bounds.y
+            }
+          }
+        };
+        annotations.push(citation);
+        console.dir(annotations);
+        var pin = $('<div style="border:1px solid green; width:10px; height:10px" class="pin"></div>');
+        pin.insertBefore();
+      });
     });
 
     // create a new selection box    
@@ -53,12 +91,8 @@
     ev.preventDefault();
     return false;
   });
-
   $(window).mouseup(function(ev) {
-
     ev.preventDefault();
     return false;
   });
-
-
 })(jQuery.noConflict());
