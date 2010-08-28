7d042f61e1ccd8bd720c6866630d000a2c5dc1f8
Elijah Insua <tmpvar@gmail.com>
2010-08-27 22:54:41 -0400

    added bookmarklet.js preprocessor

---
+++ public/js/bookmarklet.js
@@ -0,0 +1,44 @@
+(function() {
+    var loadedCount = 0;
+    var scripts  = arguments[0] || [];
+    var head = window.document.getElementsByTagName('head')[0];
+
+    var getScript=function(url,success){
+        var script= window.document.createElement('script');
+        var done  = false;
+        
+        script.src=url;
+        script.text="jQuery.noConflict( true );";
+        script.onload=script.onreadystatechange = function(){
+            if ( !done && (!this.readyState || this.readyState==='loaded' || this.readyState==='complete') ){
+                done=true;
+                success();
+            }
+        };
+        head.appendChild(script);
+    };
+
+    function preloadComplete() {
+        if (window.bookmarkletPreloaderDone) {
+            bookmarkletPreloaderDone('%_CURRENT_URL_%');
+        }
+    }
+
+    function handleLoad() {
+        loadedCount++;
+        if (loadedCount === scripts.length) {
+            preloadComplete();
+        }
+    }
+    
+    var existingScripts = document.getElementsByTagName("script");
+    
+    while(existingScripts.length)
+    {
+       existingScripts[0].parentNode.removeChild(existingScripts[0]); 
+    }
+    
+    for (var i =0; i<scripts.length; i++) {
+        getScript(scripts[i], handleLoad);
+    }
+})(['%_SCRIPTS_%']);
